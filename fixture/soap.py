54a7a8319b30bc4640366c858635b6fd2cb99c6c
Nadezhda_Sokolova <Nadezhda.Volkova@artezio.com>
2020-02-13 14:53:01 +0300

    Deleting excess code

--- fixture/soap.py
+++ fixture/soap.py
@@ -32,27 +32,3 @@ class SoapHelper(Manager):
 
 
 
-
-            response = client.service.mc_projects_get_user_accessible(username, password)
-            L = []
-            # parsed_response = json.loads(response)
-            print(response)
-
-            for project in response:
-                project = {"id": project.id, "name": project.name, 'status':project.status, 'enabled':project.enabled,
-                           'view_state':project.view_state, 'access_min':project.access_min, 'description':project.description}
-                L.append(project)
-            return json.dumps({"results":L})
-
-            # (id, name, status, inherit_global, enabled, view_state, access_min, file_path, description) = project
-                # parsed_response = json.load(Project(id=id, name=name, status=status, inherit_global=inherit_global, view_state=view_state,
-                #         description=description), 'r')
-                # print(parsed_response)
-            #     L.append()
-
-
-        except WebFault as e:
-            print(e)
-
-
-
